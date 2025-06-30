bc7917faad60fb836f34c777bab64c9301250e99
Eduardo Galvani Massino <eduardo.m.galvani@gmail.com>
2025-06-30 09:13:02 -0300

    Projeto Criado

---
+++ Features/Features.f90
@@ -0,0 +1,17 @@
+!****************************************************************************
+!
+!  PROGRAM: Features.f90
+!
+!  AUTHOR:  Eduardo Galvani Massino - eduardo.massino@usp.br
+!
+!****************************************************************************
+program Features
+   use PlotsLib, only : Scatter, Scatter2
+   implicit none
+   ! Variables
+
+   ! Body of Features
+   print *, 'Hello World'
+   read(*,*)
+
+end program Features
